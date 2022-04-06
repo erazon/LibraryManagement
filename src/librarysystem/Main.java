0b52a9575b852b61afbfe451df2a4b9f0251b8c3
Sahidul Hassan <alrazon@gmail.com>
2022-04-06 13:01:45 -0500

    code update

--- src/librarysystem/Main.java
+++ src/librarysystem/Main.java
@@ -1,24 +1,16 @@
 package librarysystem;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-
 import javax.swing.JFrame;
 
-import ui.AddMemberPanel;
-
-
-
 public class Main {
-
 	public static void main(String[] args) {
 	      EventQueue.invokeLater(() -> 
 	         {
-	            LibrarySystem.INSTANCE.setTitle("Library Application");
+	            LibrarySystem.INSTANCE.setTitle("Library Management");
 	            LibrarySystem.INSTANCE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	            
 	            LibrarySystem.INSTANCE.init();
 	            centerFrameOnDesktop(LibrarySystem.INSTANCE);
 	            LibrarySystem.INSTANCE.setVisible(true);
